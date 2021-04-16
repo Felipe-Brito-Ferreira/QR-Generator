ef1428cd82bdb12b550956b15b8367a317da42f1
Felipe Ferreira <77641195+Felipe-Brito-Ferreira@users.noreply.github.com>
2021-04-16 08:17:38 -0300

    Update QR-generator.py

--- QR-generator.py
+++ QR-generator.py
@@ -1,6 +1,6 @@
 import png
 import pyqrcode 
 fonte = input("TEXTO OU URL AQUI: ")
-file=input=("NOME PARA O SEU")
+file= input("NOME PARA O SEU")
 url = pyqrcode.create(fonte)
 url.png("MeuCodigoQR.png",scale=6)
